43b78a8d5adf36d9aa9f12ef4bfae2cf6ea502b1
Isabel Fletcher <32866320+isabelfletcher@users.noreply.github.com>
2020-10-29 16:48:08 +0000

    Create 01_extract_urbanization.R

---
+++ urbanization/01_extract_urbanization.R
@@ -0,0 +1,125 @@
+library(raster)
+library(dplyr)
+library(rgdal)
+
+venezuela <- rgdal::readOGR("ven_shp", "Parroquias_Venezuela")
+bolivar <- subset(venezuela, venezuela$ESTADO == "Bolívar")
+
+files <- list.files(path = "input/bolivar", pattern = ".tif", full.names = TRUE)
+
+######################################################
+# 1. Create new rasters of urban change 1996-2016
+revalue_ras <- function(file_1, file_2){
+  
+  # Read in file1
+  ras_1 <- file_1 %>% raster() %>% crop(extent(bolivar)) %>% raster::mask(bolivar)
+  
+  # Extract all values labelled as urban, based on legend from ESA CCI
+  ras_1 <- ras_1 == 190
+  
+  # Read in file2
+  ras_2 <- file_2 %>% raster() %>% crop(extent(bolivar)) %>% raster::mask(bolivar)
+  
+  # Extract all values labeled as urban, based on legend from ESA CCI
+  ras_2 <- ras_2 == 190
+  
+  # Loop through all cells in polygon, relabelling values 
+  for (i in 1:ncell(ras_1)){
+    
+    # Only use values not NA
+    if(is.na(ras_2[i]) == TRUE){
+    } else if (ras_2[i] == ras_1[i]){
+      # If value is the same, then revalue to 0 (no change)
+      ras_2[i] <- 0 
+      # If value is 1 then revalue to 1 (increase)
+    } else if (ras_2[i] == 1) {
+      ras_2[i] <- 1
+    } else {
+      # If else revalue to -1 (decrease)
+      ras_2[i] <- -1
+    }
+  }
+  # Save new raster to file
+  writeRaster(ras_2, gsub("input/bolivar/", paste0("output/"), file_2), overwrite = TRUE)
+}
+
+# Loop through all files
+nfiles <- length(files)-1
+for (i in 1:nfiles){
+  revalue_ras(files[i], files[i+1])
+}
+
+######################################################
+# 2. Extract area where urban cover has decreased
+
+files     <- list.files(path = "output", pattern = ".tif", full.names = TRUE)
+ras_stack <- stack(files)
+
+# Extract cells where urban has increased (1)
+ras_stack <- ras_stack == 1
+
+# Function to calculate annual total area where urban cover has decreased, for each polygon
+
+# List polygons 
+parroquias <- bolivar$PARROQUIA
+municipios <- bolivar$MUNICIPIO
+
+`%notin%` <- Negate(`%in%`)
+
+# Create empty df to fill 
+ras_df <- NULL
+
+calc_area <- function(parroquia, municipio){
+  
+  polygon     <- subset(bolivar, bolivar$PARROQUIA == parroquia & bolivar$MUNICIPIO == municipio)
+  ras_polygon <- crop(ras_stack, extent(polygon))
+  
+  # Loop through all files
+  for (i in 1:nlayers(ras_polygon)) {
+    
+    ras <- ras_polygon[[i]]
+    a   <- area(ras)
+    
+    # Ignore NA values
+    if (all(is.na(c(values(ras)))) == TRUE){
+      a_df <- data.frame(urban_increase = 0,
+                         Parroquia       = parroquia,
+                         Municipio       = municipio,
+                         Year            = gsub("X", "", names(ras)[1]),
+                         zone            = 1)
+    } else {
+      # Calculate total area where forest has decreased
+      a_df <- as.data.frame(zonal(a, ras, "sum")) %>%
+        subset(zone == 1) %>%
+        dplyr::rename(urban_increase = sum) %>% 
+        mutate(Parroquia = parroquia,
+               Municipio = municipio,
+               Year      = gsub("X", "", names(ras)[1]))
+      # Include years with no forest cover decrease
+      if (1 %notin% a_df$zone){
+        a_df <- data.frame(urban_increase = 0,
+                           Parroquia       = parroquia,
+                           Municipio       = municipio,
+                           Year            = gsub("X", "", names(ras)[1]),
+                           zone            = 1)
+      } 
+    }
+    ras_df <- rbind(ras_df, a_df)
+  }
+  ras_df <- ras_df %>% dplyr::select(-zone)
+  return(ras_df)
+}
+
+# Loop through all polygons and combine data
+data <- NULL
+for (i in 1:length(parroquias)){
+  
+  df <- calc_area(parroquias[i], municipios[i])
+  
+  data <- rbind(data, df)
+}
+
+######################################################
+# 3. Calculate cumulative forest decrease and save data
+
+data %>% mutate(urban_increase_cum = cumsum(urban_increase)) %>% write.csv("output/data.csv")
